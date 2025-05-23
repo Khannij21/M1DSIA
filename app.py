bb59fda31850b302fcdff9434945a0f8d175755a
Khannij21 <khannijIbtissam2121@gmail.com>
2025-05-23 09:44:16 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Détection du Diabète", layout="centered")
+st.title("🩺 Application de Détection du Diabète")
+
+st.markdown("Bienvenue dans l'application. Utilisez le menu à gauche pour naviguer entre les pages ")
+
